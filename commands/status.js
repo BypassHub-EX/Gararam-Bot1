999370015cd2dafa6f33df743c42e42c30493465
userAKAANS <besttieshello5@gmail.com>
2025-11-26 15:36:08 +0300

    Refactor status command reply message format

--- commands/status.js
+++ commands/status.js
@@ -1,11 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('status')
     .setDescription('Check the status of your order')
-    .addStringOption(opt => opt.setName('order').setDescription('Order ID').setRequired(true)),
+    .addStringOption(opt =>
+      opt.setName('order')
+        .setDescription('Order ID')
+        .setRequired(true)
+    ),
+
   async execute(interaction) {
     const orderId = interaction.options.getString('order');
-    await interaction.reply({ content: `📦 Order #${orderId} is currently being processed. You will receive your delivery shortly.`, ephemeral: true });
+
+    await interaction.reply({
+      content: `Order #${orderId} is being processed. You will receive your delivery soon.`,
+      ephemeral: true
+    });
   }
 };
